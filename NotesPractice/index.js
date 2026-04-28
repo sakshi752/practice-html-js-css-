64a2936b1fb39a635003fde0682667eba25a1942
Sakshi <saks0705@gmail.com>
2026-04-28 18:14:02 +0530

    noteProject-add,delete,localStorage

--- NotesPractice/index.js
+++ NotesPractice/index.js
@@ -2,7 +2,16 @@ const toggleBtns = document.querySelectorAll('.toggle');
 const formSection = document.querySelector('.formSection');
 const form = document.querySelector('#addNote');
 const noteSection = document.querySelector('#noteSection');
+let notes = [];
+let editTodo = null;
 
+window.onload = () => {
+    notes = JSON.parse(localStorage.getItem("notes")) || [];
+    renderNote()
+
+}
+
+// for modal logic
 toggleBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         if (formSection.style.display === "none") {
@@ -19,56 +28,120 @@ formSection.addEventListener("click", (e) => {
     }
 });
 
+// on submit
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const title = document.querySelector('#title').value;
     const category = document.querySelector('#category').value;
     const date = document.querySelector('#date').value;
-    const desc = document.querySelector('#desc').value;
+    let desc = document.querySelector('#desc').value;
 
     if (title.trim() === "" || date.trim() === "") {
         alert("Enter fields properly")
     }
 
-    if (desc.trim === "") {
+    if (desc.trim() === "") {
         desc = "No description for this note";
     }
     const formattedDate = new Date(date).toLocaleDateString();
+    const categories = ["career", "health", "hobbies"];
 
-    console.log(title, category, formattedDate, desc);
-    createNote(title, category, formattedDate, desc);
+    if (editTodo) {
 
-    
+    } else {
+        addToArray(title, categories[category], formattedDate, desc);
+        renderNote();
+        addToLocalStorage();
+
+    }
+
+    form.reset();
+    formSection.style.display = "none"
 })
 
-const createNote = (title, category, date, desc) => {
+const addToArray = (title, category, date, desc) => {
+    const categories = ["career", "health", "hobbies"];
+    notes.push({
+        id: Date.now(),
+        title,
+        desc,
+        category,
+        date
+    })
+}
+
+const renderNote = () => {
+    noteSection.innerHTML = "";
+
+    notes.forEach(note => {
+        createNote(note);
+    });
+}
+
+const createNote = (noteData) => {
+
     const note = document.createElement("div");
     note.classList.add("note");
 
+
     const noteTitle = document.createElement("div");
-    noteTitle.innerHTML = `   <p>${title}</p>
-                    <span>${date}</span>`
     noteTitle.classList.add("noteTitle");
 
+    noteTitle.innerHTML = `
+        <p>${noteData.title}</p>
+        <span>${noteData.date}</span>
+    `;
+
+
+    const noteDesc = document.createElement("div");
+    noteDesc.classList.add("noteDesc");
+
+    const description = document.createElement("p");
+    description.textContent = noteData.desc;
+
+    noteDesc.appendChild(description);
+
+
     const subInfo = document.createElement("div");
-    subInfo.innerHTML = ` <div class="noteBtns">
-                <span class="delete"><i class="fa-solid fa-trash"></i></span>
-                <span class="edit"><i class="fa-solid fa-pen-to-square"></i></span>
-            </div>
-            <span>${category}</span>`
     subInfo.classList.add("subInfo");
 
-    const noteDesc = document.createElement("div")
-    noteDesc.classList.add("noteDesc");
-    const description = document.createElement("p");
-    description.innerText = desc;
+    subInfo.innerHTML = `
+        <div class="noteBtns">
+            <span class="delete" data-id="${noteData.id}">
+                <i class="fa-solid fa-trash"></i>
+            </span>
+
+            <span class="edit" data-id="${noteData.id}">
+                <i class="fa-solid fa-pen-to-square"></i>
+            </span>
+        </div>
+
+        <span>${noteData.category}</span>
+    `;
+
 
     note.appendChild(noteTitle);
     note.appendChild(noteDesc);
     note.appendChild(subInfo);
-    noteSection.appendChild(note);
 
-    
+    noteSection.appendChild(note);
 }
 
-const addToArray =()=>{}
+const addToLocalStorage = () => {
+
+    localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+noteSection.addEventListener("click", (e) => {
+    const span = e.target.closest("span");
+    if (!span) return;
+    // const noteDiv = e.target.closest(".note");
+    const id = Number(span.dataset.id);
+    if (span.classList.contains("delete")) {
+        notes = notes.filter(note => note.id != id);
+        addToLocalStorage();
+        renderNote();
+    } else {
+        // edit note
+    }
+})
